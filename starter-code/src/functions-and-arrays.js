2e3d1c234dbd7dfa2c884c9d9aead6659abf31ff
Cynthia Takiishi <cynthiaakemit@gmail.com>
2020-07-10 03:58:33 -0300

    Functions and Arrays Exercise

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,195 +1,307 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers(num1, num2) {
-  if (num1 > num2) {
-    return num1;
+function maxOfTwoNumbers(number1, number2) {
+  if (number1 >= number2) {
+      return number1
   }
-  return num2;
+
+  return number2
 }
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord(words) {
-  if (words.length === 0){
-    return null;
-  } else {
-  let longest = words[0];
-  words.forEach(element => {
-    if (longest.length < element.length) {
-      longest = element;
-    }
-  });
-  return longest;
-  } 
+function findLongestWord(arrayWords) {
+  let result = '';
+
+  if (arrayWords.length === 0){
+      return null
+  }
+  
+  for (let i=0; i<arrayWords.length; i++) {
+      if (arrayWords[i].length > result.length) {
+          result = arrayWords[i]
+      }
+  }
+
+  return result
 }
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumArray(array) {
-  let sum = 0;
-  array.forEach(element => {
-    sum += element;
-  });
-  return sum;
+function sumNumbers(arrayNumber) {
+  let result = 0;
+
+  for (let i=0; i<arrayNumber.length; i++) {
+      result = result + arrayNumber[i]
+  }
+
+  return result
 }
+
+//Bonus 3.1
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+
+ function sum(arrayMixed) {
+   let result = 0;
+ 
+   for (let i=0; i<arrayMixed.length; i++) {
+     if (typeof arrayMixed[i] === 'object'){
+      throw new Error("Unsupported data type sir or ma'am")
+     }
+
+     if (typeof arrayMixed[i] === 'string') {
+       result += arrayMixed[i].length
+     } else if (arrayMixed[i] === true) {
+       result += 1
+     } else {
+       result += arrayMixed[i]
+     }
+   }
+
+   return result
+ }
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers(array) {
-  if (array.length === 0) {
-    return null;
-  } else{
-  return sumArray(array) / array.length;
+function averageNumbers(arrayNumber) {
+  let result = 0;
+
+  if (arrayNumber.length === 0) {
+    return null
   }
+
+  result = sumNumbers(arrayNumber) / arrayNumber.length
+
+  return result
+}
+ 
+// Level 2: Array of strings
+const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+function averageWordLength(arrayString) {
+  let result = 0;
+
+  if (arrayString.length === 0) {
+    return null
+  }
+
+  for (let i=0; i<arrayString.length; i++ ) {
+    result += arrayString[i].length
+  }
+
+  result = result / arrayString.length
+
+  return result
 }
 
-// Level 2: Array of strings
-const wordsArr = [
-  'seat',
-  'correspond',
-  'linen',
-  'motif',
-  'hole',
-  'smell',
-  'smart',
-  'chaos',
-  'fuel',
-  'palace'
-];
+// Bonus 4.1
+const mixedArr1 = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
 
-function averageWordLength(array) {
-  let arrayLength = [];
-  array.forEach(element => {
-    arrayLength.push(element.length);
-  })
-  return averageNumbers(arrayLength);
+function avg(arr) {
+  let result = 0
+
+  if (arr.length === 0){
+    return null
+   }
+
+   for (let i=0; i<arr.length; i++) {
+     if (typeof arr[i] === 'string') {
+       result += arr[i].length
+     } else if (arr[i] === true) {
+       result += 1
+     } else {
+       result += arr[i]
+     }
+   }
+
+   result = +(result / arr.length).toFixed(2)
+
+   return result
 }
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
-  'crab',
-  'poison',
-  'contagious',
-  'simple',
-  'bring',
-  'sharp',
-  'playground',
-  'poison',
-  'communion',
-  'simple',
-  'bring'
+'crab',
+'poison',
+'contagious',
+'simple',
+'bring',
+'sharp',
+'playground',
+'poison',
+'communion',
+'simple',
+'bring'
 ];
 
-function uniquifyArray(array) {
-  uniqueArray = [];
-  array.forEach(element => {
-    if (uniqueArray.indexOf(element) === -1) {
-      uniqueArray.push(element);
+function uniquifyArray(arrayString) {
+  let newArray = []
+
+  if (arrayString.length === 0) {
+    return null
+  }
+
+  for (let i=0; i<arrayString.length; i++) {
+    compare = arrayString.indexOf(arrayString[i])
+    if (i === compare) {
+      newArray.push(arrayString[i])
     }
-  });
-  return uniqueArray;
+  }
+
+  return newArray
 }
 
 // Iteration #6: Find elements
-const wordsFind = [
-  'machine',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'truth',
-  'disobedience'
-];
+const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist(array, word) {
-  let exists = false;
-  array.forEach(element => {
+function doesWordExist(arrayString, word) {
+  let result = false;
+
+  if (arrayString.length === 0) {
+    return null
+  }
+  
+  arrayString.forEach(element => {
     if (element === word) {
-      exists = true;
+      result = true
     }
-  });
-  return exists;
+  })
+
+  return result
 }
 
 // Iteration #7: Count repetition
 const wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
+'machine',
+'matter',
+'subset',
+'trouble',
+'starting',
+'matter',
+'eating',
+'matter',
+'truth',
+'disobedience',
+'matter'
 ];
 
-function howManyTimes(array, word) {
-  let times = 0;
-  array.forEach(element => {
+function howManyTimes(arrayString, word) {
+  let result = 0;
+  
+  arrayString.forEach(element => {
     if (element === word) {
-      times++;
+      result += 1
     }
-  });
-  return times;
+  })
+
+  return result
 }
 
 // Iteration #8: Bonus
-
 const matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+[8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
+[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
+[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+[52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
+[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+[24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+[67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
+[24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+[21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
+[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
+[16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+[86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+[19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+[4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+[88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+[4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
+[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
+[20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+[1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
+function greatestProduct(matrix) {
+  let productY = 0;
+  let productX = 0;
+  let resultY = 0;
+  let resultX = 0;
+  let result = 0;
 
-function greatestProduct(matrixOfNumbers){
-  let max=0
-  let result=0
-  for (let i=0;i<matrixOfNumbers.length;i++){
-    for (let j=0;j<matrixOfNumbers[i].length;j++){
-      if((j-3)>=0){
-        result=(matrixOfNumbers[i][j] * matrixOfNumbers[i][j - 1] * matrixOfNumbers[i][j - 2] * matrixOfNumbers[i][j - 3])
-        if (max<result){
-          max=result
-        }
+  for (let i=0; i<matrix.length; i++) {
+    for (let j=0; j<matrix[i].length-3; j++) {
+
+      productY = matrix[i][j]*matrix[i][j+1]*matrix[i][j+2]*matrix[i][j+3]
+
+      if (productY > resultY) {
+        resultY = productY
       }
-      if((i-3)>=0){
-        result=(matrixOfNumbers[i][j] * matrixOfNumbers[i-1][j] * matrixOfNumbers[i-2][j] * matrixOfNumbers[i-3][j])
-        if (max<result){
-          max=result
-        }
-      }
-      if((i-3)>=0 && (j-3)>=0) {
-        result=(matrixOfNumbers[i][j] * matrixOfNumbers[i-1][j - 1] * matrixOfNumbers[i-2][j - 2] * matrixOfNumbers[i-3][j - 3])
-        if (max<result){
-          max=result
-        }
-      }
+
     }
   }
-  return max
+
+  for (let i=0; i<matrix.length-3; i++) {
+    for (let j=0; j<matrix[i].length; j++) {
+
+      productX = matrix[i][j]*matrix[i+1][j]*matrix[i+2][j]*matrix[i+3][j]
+
+      if (productX > resultX) {
+        resultX = productX
+      }
+
+    }
+  }
+
+  if (resultY > resultX) {
+    result = resultY
+  } else {
+    result = resultX
+  }
+
+  return result
+}
+
+//Bonus 8.1
+function greatestProductOfDiagonal(matrix) {
+  let productDiag = 0;
+  let productInv = 0;
+  let resultDiag = 0;
+  let resultInv = 0;
+  let result = 0;
+
+  for (let i=0; i<matrix.length-3; i++) {
+    for (let j=0; j<matrix[i].length-3; j++) {
+
+      productDiag = matrix[i][j]*matrix[i+1][j+1]*matrix[i+2][j+2]*matrix[i+3][j+3]
+
+      if (productDiag > resultDiag) {
+        resultDiag = productDiag
+      }
+
+    }
+  }
+
+  for (let i=matrix.length-1; i<3; i--) {
+    for (let j=0; j<matrix[i].length-3; j++) {
+
+      productInv = matrix[i][j]*matrix[i-1][j+1]*matrix[i-2][j+2]*matrix[i-3][j+3]
+      console.log(productInv)
+
+      if (productInv > resultInv) {
+        resultInv = productInv
+      }
+
+    }
+  }
+
+  if (resultDiag > resultInv) {
+    result = resultDiag
+  } else {
+    result = resultInv
+  }
+
+  return result
 }
